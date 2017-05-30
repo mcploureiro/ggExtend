89506c6e3c3d57694146443646c15a6c3f141aa5
mhe3 <mike.github2@gmail.com>
2017-05-30 10:16:29 -0400

    Created flattenList
    Flattens nested lists, does not yet currently support flattening to a named list.

---
+++ flattenList.cpp
@@ -0,0 +1,23 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+// Enable C++11 via this plugin (Rcpp 0.10.3 or later)
+// [[Rcpp::plugins(cpp11)]]
+
+// [[Rcpp::export]]
+std::list<SEXP> flattenList(List x){
+  std::list<SEXP> o(0);
+  std::size_t input_size = x.size();
+  
+  for(std::size_t i = 0; i < input_size; i++)
+    {
+    if (TYPEOF(x[i]) == VECSXP)
+      {
+      o.splice(o.end(),test(x[i]));
+    } else 
+      {
+      o.push_back(x[i]);
+    }
+  }
+
+  return o;
+}
