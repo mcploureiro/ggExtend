df00c5a41ec88d0904e0fbbdb442929b822e910b
mhe3 <mike.github2@gmail.com>
2017-06-16 14:14:15 -0400

    Added infix operator %+safe
    Added infix operator that "safely" adds ggplot themes together. It will only add theme elements if they are not already defined.

--- infix-operators.R
+++ infix-operators.R
@@ -22,3 +22,16 @@ getEnvi <- function(i) {
 
 #dt <- as.data.table(df)
 #dt[, c("day" %--% "price", "price3" %--% "y")]
+
+#-------------------------------------------------------------------------------------------------------------------------------------#
+##Infix function to "safely" add ggplot themes together. This only adds themes that are not already set and thus can be considered "safe".
+##I.e. a theme element will only be added if it is not already NULL
+`%+safe%` <- function(e1, e2){
+  if (!is.theme(e1) || !is.theme(e2)) {
+    stop("%+replace% requires two theme objects", call. = FALSE)
+  }
+
+  not_in_e1 <- names(e2)[!names(e2) %in% names(e1)]
+  e1[not_in_e1] <- e2[not_in_e1]
+  e1
+}
